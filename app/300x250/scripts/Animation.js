6134878cf4b5b9868f0d7f864bbdff231739cd25
tim.hennessey <Tim.Hennessey@deutschinc.com>
2019-05-16 15:39:29 -0700

    Initial Commit

---
+++ app/300x250/scripts/Animation.js
@@ -0,0 +1,74 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	// var link = "https://www.realcaliforniamilk.com/product-locator?MM=panel2&ZIP=90807";
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+		t.set("#bg", {scale:1.3, opacity:1});
+		t.set("#caption-1", {opacity:0, y:50});
+		t.set("#caption-2", {opacity:0, y:50});
+		t.set("#caption-3", {opacity:0, y:50});
+		t.set("#cta", {opacity:0});
+		t.set("#cta-over", {opacity:0});
+
+		banner.addEventListener('mouseover', handleOver, true);
+		banner.addEventListener('mouseout', handleOut, true);
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+		t.delayedCall(0.0, step1);
+		t.delayedCall(1.0, step2);
+	}
+
+	// --------------------------------------------------------------------------------------
+	function step1() {
+		t.to("#caption-1", 1, {opacity:1, y:0, delay:0.0, ease:Cubic.easeOut});
+		t.to("#bg", 1, {opacity:1, delay:0.0, ease:Cubic.easeInOut});
+		t.to("#bg", 10, {scale:1, delay:0.0, ease:Linear.easeNone});
+	}
+
+	// --------------------------------------------------------------------------------------
+	function step2() {
+
+		t.to("#caption-2", 1, {opacity:1, y:0, delay:0.2, ease:Cubic.easeOut});
+		t.to("#caption-3", 1, {opacity:1, y:0, delay:0.4, ease:Cubic.easeOut});
+		t.to("#cta", 1, {opacity:1, delay:1, ease:Cubic.easeInOut});
+	}
+
+	// --------------------------------------------------------------------------------------
+	function handleOver() {
+		// TweenLite.to("#legal", 0.2, {opacity:1, ease:Cubic.easeInOut});
+		TweenLite.to("#cta-over", 0.2, {opacity:1, ease:Cubic.easeInOut});
+	}
+
+	// --------------------------------------------------------------------------------------
+	function handleOut() {
+		// TweenLite.to("#legal", 0.5, {opacity:0, ease:Cubic.easeInOut});
+		TweenLite.to("#cta-over", 0.5, {opacity:0, ease:Cubic.easeInOut});
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
